4d127254392611dfcc8c72a560789e1f40a2991b
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2021-12-18 02:20:34 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
